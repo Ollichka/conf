89e95bc9925059115bce5b828585450b33aa4eb7
Ollichka <salnikova.olia@gmail.com>
2016-02-14 14:33:45 +0200

    login

--- src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
+++ src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
@@ -48,8 +48,7 @@ public class ConferenceApi {
 
     // TODO 1 Pass the ProfileForm parameter
     // TODO 2 Pass the User parameter
-    public Profile saveProfile() throws UnauthorizedException {
-
+    public Profile saveProfile(User user,ProfileForm pr) throws UnauthorizedException {
         String userId = null;
         String mainEmail = null;
         String displayName = "Your name will go here";
@@ -57,22 +56,27 @@ public class ConferenceApi {
 
         // TODO 2
         // If the user is not logged in, throw an UnauthorizedException
+        if (user == null) {
+            throw new UnauthorizedException("Authorization required");
+        }
 
         // TODO 1
         // Set the teeShirtSize to the value sent by the ProfileForm, if sent
         // otherwise leave it as the default value
 
+        teeShirtSize =  pr.getTeeShirtSize();
         // TODO 1
         // Set the displayName to the value sent by the ProfileForm, if sent
         // otherwise set it to null
-
+        displayName = pr.getDisplayName();
         // TODO 2
         // Get the userId and mainEmail
-
+        userId = user.getUserId();
+        mainEmail = user.getEmail();
         // TODO 2
         // If the displayName is null, set it to default value based on the user's email
         // by calling extractDefaultDisplayNameFromEmail(...)
-
+        if(displayName==null) displayName = extractDefaultDisplayNameFromEmail(mainEmail);
         // Create a new Profile entity from the
         // userId, displayName, mainEmail and teeShirtSize
         Profile profile = new Profile(userId, displayName, mainEmail, teeShirtSize);
